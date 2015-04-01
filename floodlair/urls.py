d30578b1751ebb2f5e761a930b7463f7f0798689
Aleksey 'LXj' Alekseyev <gotletter@gmail.com>
2015-04-01 20:17:04 +0300

    Kill all humans

--- floodlair/urls.py
+++ floodlair/urls.py
@@ -4,6 +4,7 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.conf import settings
 from django.conf.urls.static import static
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
 
 admin.autodiscover()
@@ -12,6 +13,7 @@ urlpatterns = patterns('',
     url(r'^$', 'apps.irc.views.main', name="home_page"),
     url(r'^(\d{2})\.(\d{2})\.(\d{4})/$', 'apps.irc.views.main'),
     url(r'^webirc/$', 'apps.irc.views.webirc', name="webirc"),
+    url(r'^robots.txt$', TemplateView.as_view(template_name='robots.txt'))
     url(r'^admin/', include(admin.site.urls)),
 )
 
