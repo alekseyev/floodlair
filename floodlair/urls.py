250a7c00aeeff2d863ddbe9b98be675c6b52a61c
Aleksey 'LXj' Alekseyev <gotletter@gmail.com>
2015-04-01 20:22:33 +0300

    Proper content type for robots

--- floodlair/urls.py
+++ floodlair/urls.py
@@ -13,7 +13,8 @@ urlpatterns = patterns('',
     url(r'^$', 'apps.irc.views.main', name="home_page"),
     url(r'^(\d{2})\.(\d{2})\.(\d{4})/$', 'apps.irc.views.main'),
     url(r'^webirc/$', 'apps.irc.views.webirc', name="webirc"),
-    url(r'^robots.txt$', TemplateView.as_view(template_name='robots.txt')),
+    url(r'^robots.txt$', TemplateView.as_view(
+        template_name='robots.txt', content_type="text/plain")),
     url(r'^admin/', include(admin.site.urls)),
 )
 
